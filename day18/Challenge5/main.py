e579695b0795b4865514372f253a4ed2534c963c
mgiammal <mjgiammalvo@gmail.com>
2022-12-21 09:46:05 -0500

    Challenge 5 on Day 18

--- day18/Challenge5/main.py
+++ day18/Challenge5/main.py
@@ -2,14 +2,24 @@ import turtle as t
 import random
 
 tim = t.Turtle()
-tim.pen(pensize=10)
-tim.speed("fast")
+t.colormode(255)
+t.speed("fastest")
+screen = t.Screen()
 
-########### Challenge 4 - Random Walk ########
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
-           "SeaGreen"]
 
-for _ in range(random.randint(0, 1000)):
-    tim.color(random.choice(colours))
-    tim.seth(random.randrange(0, 360, 90))
-    tim.forward(35)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+########### Challenge 5 - Spirograph ########
+
+
+for _ in range(0, 360, 5):
+    t.color(random_color())
+    t.circle(radius=100)
+    t.seth(_)
+
+screen.exitonclick()
